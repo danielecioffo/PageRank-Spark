f0f34ff26b17378fba9435f4c6d8221411296e02
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-05-28 15:34:30 +0200

    try with additional join

--- pagerank/page_rank.py
+++ pagerank/page_rank.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     # import context from Spark (distributed computing using yarn, name of the application)
-    conf = SparkConf().setMaster("yarn").setAppName("page_rank_baggins").set("spark.dynamicAllocation.enabled", True)
+    conf = SparkConf().setMaster("yarn").setAppName("page_rank_baggins").set("spark.dynamicAllocation.enabled", "true")
     sc = SparkContext(conf=conf)
 
     # import input data from txt file to rdd
